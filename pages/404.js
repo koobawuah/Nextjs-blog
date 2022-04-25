f620467b00a0d7fead161706f4bf45680e43d10a
koobawuah <me@bawuahboakye.com>
2022-04-25 23:37:39 +0000

    Corrected unwanted lines of funny characters popping up from prettier formatting

--- pages/404.js
+++ pages/404.js
@@ -4,15 +4,14 @@ const NotFound = () => {
   return (
     <div className="w-full mx-auto">
       <h1 className="text-4xl font-semibold tracking-tighter text-red-700">
-        {" "}
-        Page Not Found!{" "}
+        Page Not Found!
       </h1>
       <p className="pt-6 text-base tracking-normal">
-        Nextjs Blog found no page :({" "}
+        Nextjs Blog found no page :(
       </p>
       <Link href="/">
         <a className="font-semibold tracking-tighter underline text-md">
-          Go back home{" "}
+          Go back home
         </a>
       </Link>
     </div>
